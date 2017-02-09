38c9b1e10312c8a5f4f2af1fc95d4303ab0abb90
Amit Auddy <amitauddy94@gmail.com>
2017-02-09 23:14:01 +0530

    created server with config.json

---
+++ server_with_configfile/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const http = require('http');
+const express = require('express');
+const fs = require('fs');
+
+const configJson = fs.readFileSync("./server_config/config.json");
+const config = JSON.parse(configJson);
+
+const app = express();
+app.use(express.static(config.webServer.staticFolder));
+const httpServer = http.createServer(app);
+
+httpServer.listen(config.webServer.port , function(err){
+    if (err) {
+        console.log("error creating server!");
+        return;
+    }
+    console.log(`server listening on port ${config.webServer.port}...........`);
+});
