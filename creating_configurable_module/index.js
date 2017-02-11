4c01f9a1a679ea678e3f08a15f99b7d4d4c417cc
Amit Auddy <amitauddy94@gmail.com>
2017-02-11 17:43:49 +0530

    added semicolon

--- creating_configurable_module/index.js
+++ creating_configurable_module/index.js
@@ -3,7 +3,7 @@
 const configurableModule = require('./configurable_module');
 
 const configApp_1 = configurableModule({configPrefix: '$ '});
-const configApp_2 = configurableModule({configPrefix: '# '})
+const configApp_2 = configurableModule({configPrefix: '# '});
 
 configApp_1.log('normal user!');
 configApp_2.log('super user!');
